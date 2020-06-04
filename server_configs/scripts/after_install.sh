f472759fcac20d249c19b3f7ae2a49811b45ee2c
alienx456 <estebanelias27@gmail.com>
2020-06-04 16:16:32 -0500

    Actualizado despliegue

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Install libaries
-# cd /home/ubuntu
-# virtualenv -p python3 venv
-# source venv/bin/activate
-# pip install -r requirements.txt
+cd /home/ubuntu
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
 # python manage.py collectstatic --no-input
 
 echo "Ready for after_install"
