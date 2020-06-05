8b01f595233391da606a787295d61bcb2b230ba2
alienx456 <estebanelias27@gmail.com>
2020-06-04 22:56:11 -0500

    actualizado

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Install libaries
-
+source /home/ubuntu/.bashrc
 cd /home/ubuntu/despliegue
-source home/ubuntu/miniconda3/etc/profile.d/conda.sh
-/home/ubuntu/miniconda3/bin/conda create --force --name djangopa python=3.6
-/home/ubuntu/miniconda3/bin/conda activate djangopa
+#source home/ubuntu/miniconda3/etc/profile.d/conda.sh
+conda env remove -name djangopa
+conda create --name djangopa python=3.6
+conda activate djangopa
 pip install -r requirements.txt
 sudo a2ensite accidentesaereros-backend.conf
 sudo /etc/init.d/apache2 restart
