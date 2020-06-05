ccba1a921b090a10bf597b59a18060c253836db7
alienx456 <estebanelias27@gmail.com>
2020-06-05 15:44:42 -0500

    a

--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install apache2
-pip3 install virtualenv
-sudo apt install libapache2-mod-wsgi-py3
-sudo apt install libpq-dev python-dev
+sudo apt -y install apache2
+pip3 install -y virtualenv
+sudo apt install -y libapache2-mod-wsgi-py3
+sudo apt install -y libpq-dev python-dev
