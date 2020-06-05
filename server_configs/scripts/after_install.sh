21f889040244caa376ae8978245770a76756cd8a
alienx456 <estebanelias27@gmail.com>
2020-06-04 21:44:50 -0500

    Actualizacion despliegue

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -4,6 +4,9 @@
 
 cd /home/ubuntu/despliegue
 pwd
+echo "-------------------------------------------------------------------"
+echo "-------------------------------------------------------------------"
+which virtualenv
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
