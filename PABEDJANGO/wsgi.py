fc2d8ee91a6e1c0b0b02b7efcba92de1fb152ac3
alienx456 <estebanelias27@gmail.com>
2020-06-05 10:57:20 -0500

    actualizado wsgi

--- PABEDJANGO/wsgi.py
+++ PABEDJANGO/wsgi.py
@@ -17,7 +17,7 @@ python_home = '/home/ubuntu/despliegue/venv'
 
 activate_this = python_home + '/bin/activate_this.py'
 
-exec(activate_this, dict(__file__=activate_this))
+exec(compile(open(activate_this "rb").read(), activate_this, 'exec'), dict(__file__=activate_this))
 
 
 from django.core.wsgi import get_wsgi_application
