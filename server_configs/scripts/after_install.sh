913d0e681510ffbad65a84df0bb12509aa6f2893
alienx456 <estebanelias27@gmail.com>
2020-06-04 23:37:04 -0500

    again on virtualenv

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Install libaries
-echo "------------------------------------------------------------------------"
-source /home/ubuntu/.bashrc
 cd /home/ubuntu/despliegue
-source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
-/home/ubuntu/miniconda3/bin/conda remove -name djangopa
-/home/ubuntu/miniconda3/bin/conda create --name djangopa python=3.6
-/home/ubuntu/miniconda3/bin/conda activate djangopa
+pwd
+echo "-------------------------------------------------------------------"
+echo "-------------------------------------------------------------------"
+which virtualenv
+/home/ubuntu/.local/bin/virtualenv -p python3 venv
+source venv/bin/activate
 pip install -r requirements.txt
+# python manage.py collectstatic --no-input
 sudo a2ensite accidentesaereros-backend.conf
 sudo /etc/init.d/apache2 restart
 
--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -2,4 +2,5 @@
 
 sudo apt update
 sudo apt install apache2
+pip3 install virtualenv
 sudo apt install libapache2-mod-wsgi-py3
