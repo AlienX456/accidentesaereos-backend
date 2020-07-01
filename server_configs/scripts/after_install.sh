d337a86b88a60c09ed2991549214f9af9757e3e8
alienx456 <estebanelias27dev@gmail.com>
2020-07-01 10:45:50 -0500

    changed docker management - temp

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -1,4 +1,5 @@
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=alienx456/accidentesaereos-backend:latest --format="{{.ID}}"))
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 . /home/ubuntu/enviroment/accidentesbackend-vars.sh
 echo 'Database var $DATABASE_PORT'
 docker run -e SECRET_KEY=$SECRET_KEY -e DATABASE_NAME=$DATABASE_NAME -e DATABASE_USER=$DATABASE_USER -e DATABASE_PASS=$DATABASE_PASS -e  DATABASE_HOST=$DATABASE_HOST -e DATABASE_PORT=$DATABASE_PORT -itd -p 8000:8000 alienx456/accidentesaereos-backend:latest
