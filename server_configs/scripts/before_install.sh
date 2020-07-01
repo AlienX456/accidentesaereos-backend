b4b6e51cd8db9461e384586c3b387b18e97fcc03
alienx456 <estebanelias27dev@gmail.com>
2020-07-01 00:40:43 -0500

    build spec examine

--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
 
-sudo apt update
-sudo apt -y install apache2
-pip3 install virtualenv
-sudo apt install -y libapache2-mod-wsgi-py3
-sudo apt install -y libpq-dev python-dev
-echo '---------------Before_install.sh Terminated---------------'
